fba27a0b911882e5a1c7715e3cdbb37e55bdb7e2
NessimTarchoun <NessimTarchoun@gmail.com>
2021-06-29 13:52:12 +0200

    29/06

--- API/api.py
+++ API/api.py
@@ -1,10 +1,12 @@
+import sys
+import os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__),os.path.pardir)))
 import flask
-from flask import request, jsonify
+from flask import request
 import json
 
 app = flask.Flask(__name__)
 app.config["DEBUG"] = True
-
 
 
 @app.route('/', methods=['GET'])
@@ -12,46 +14,46 @@ def home():
     return '''<h1>Event log API</h1>'''
 
 # A route to return all of the available entries in our catalog.
-@app.route('/events/all', methods=['GET'])
-def api_all():
-    with open('events.json') as json_file:
-        extracted_events=json.load(json_file)
-    return (extracted_events)
+#@app.route('/events/', methods=['GET'])
+#def api_all():
+#    with open('events.json') as json_file:
+  #      extracted_events=json.load(json_file)
+ #   return (extracted_events)
 
-
-@app.route('/events', methods=['GET'])
-def api_id():
-    li_st=[]
-    with open('events.json') as json_file:
+@app.route('/events/', methods=['GET'])
+def api_searching_by_id():
+    with open('../events.json') as json_file:
         events=json.load(json_file)
 
+    if 'ts1' in request.args and 'ts2' in request.args and 'id' in request.args:
+        timestamp1 = int(request.args['ts1'])
+        timestamp2 = int(request.args['ts2'])
+        wanted_class = int(request.args['id'])
+        common_list=[]
+        for i in range (len(events)):
+            if ((events[str(i)]['timestamp']) >= timestamp1) and ((events[str(i)]['timestamp']) <= timestamp2) and ((events[str(i)]['class_of_operation']) == wanted_class):
+                common_list.append(events[str(i)])
+        return (json.dumps(common_list))
+    
+    
     if 'id' in request.args:
         wanted_class = int(request.args['id'])
-    else:
-        return "Error: No id field provided. Please specify an id."
+        id_list=[]
+        for i in range (len(events)):
+            if (events[str(i)]['class_of_operation']) == wanted_class:
+                id_list.append(events[str(i)])
+        return(json.dumps(id_list))
+    
+    elif 'ts1' in request.args and 'ts2' in request.args:
+        timestamp1 = int(request.args['ts1'])
+        timestamp2 = int(request.args['ts2'])
+        ts_list=[]
+        for i in range (len(events)):
+            if ((events[str(i)]['timestamp']) >= timestamp1) and((events[str(i)]['timestamp']) <= timestamp2) :
+                ts_list.append(events[str(i)])
+        return(json.dumps(ts_list))
 
-    for i in range (len(events)):
-        if (events[str(i)]['class_of_operation']) == wanted_class:
-            li_st.append(events[str(i)])
-    list_json= json.dumps(li_st)
-    return (list_json)
+    else:
+        return (json.dumps(events))
 
 app.run()
-
-def draft_function():
-    with open('events.json') as json_file:
-        events=json.load(json_file)
-    #print(events)
-    list=[]
-    wanted_class= 10
-    for i in range (len(events)):
-          #print (len(events))
-        if (events[str(i)]['class_of_operation']) == wanted_class:
-            list.append(events[str(i)])
-
-    # Use the jsonify function from Flask to convert our list of
-    # Python dictionaries to the JSON format.
-    print (list)
-      #print events[i]
-
-api_all()  
